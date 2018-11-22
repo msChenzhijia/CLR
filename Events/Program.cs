8d1fde5e26733a669afdca10b513d5871fefc40e
Chen <280360239@qq.com>
2018-11-22 23:27:36 +0800

    新增了约束

--- Events/Program.cs
+++ Events/Program.cs
@@ -41,4 +41,20 @@ namespace Events
     {
         public event EventHandler<NewMailEventArgs> NewMail;
     }
+    internal sealed class Fax
+    {
+        //将MailManager对象传给构造器
+        public Fax(MailManager mm)
+        {
+            //构造EventHandler<NewMailEventArgs>委托的一个实例
+            //使用它引用我们的FaxMsg回调方法
+            //向MailManager的NewMail事件登记我们的回调方法
+            mm.NewMail += FaxMsg;
+        }
+        private void FaxMsg(Object o,NewMailEventArgs e)
+        {
+            //'sender'表示MailManager,便于将信息传回给它
+            //'e'表示表示MailManager对象想传给我们的附加事件信息
+        }
+    }
 }
---
+++ 约束/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 约束
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var sl=new  PrimaryConstraintOfStram<FileStream>();
+            
+        }
+        
+
+        #region 次要约束
+        private static List<TBase> ConverIList<T, TBase>(IList<T> list) where T : TBase
+        {
+            List<TBase> baseList = new List<TBase>(list.Count);
+            for (Int32 index = 0; index < list.Count; index++)
+            {
+                baseList.Add(list[index]);
+            }
+            return baseList;
+        }
+
+        public static void CallingConverIList()
+        {
+            //构造并初始化List<String>(它实现了IList<String>)
+            IList<String> ls = new List<String>();
+            ls.Add("A String");
+            //将IList<String>转换成一个IList<Object> String是Object的派生类
+            IList<Object> lo = ConverIList<String, Object>(ls);
+            //将IList<String>转换成IList<IComparable>   String继承了IComparable
+            IList<IComparable> lc = ConverIList<String, IComparable>(ls);
+            //将IList<String>转换成一个IList<IComparable<String>>String继承了IComparable
+            IList<IComparable<String>> lcs = ConverIList<String, IComparable<String>>(ls);
+            //将IList<String>转换成一个IList<<String>返回同一个类型
+            IList<String> ls2 = ConverIList<String, String>(ls);
+            //将IList<String>转换成一个IList<<Exception>String 和Exception没任何关系
+            //IList<String> le = ConverIList<String, Exception>(ls);错误
+
+        }
+        #endregion
+
+    }
+    #region 主要约束
+    /// <summary>
+    /// T要么是Stream或是Stram的派生类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal sealed class PrimaryConstraintOfStram<T> where T : Stream
+    {
+        public void M(T stream)
+        {
+            stream.Close();
+        }
+    }
+    internal sealed class PrimaryConstraintOfClass<T>where T : class
+    {
+        T temp = null;
+    }
+    internal sealed class PrimaryConstraintOfStruct<T>where T : struct
+    {
+        public static T Factory()
+        {
+            return new T();
+        }
+    }
+    #endregion
+    #region  构造器约束
+
+    #endregion
+}
